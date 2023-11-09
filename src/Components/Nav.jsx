06892b348a7107f7eb5d880ef1cf1b9e7fb4571f
David Moller <davidmollera@hotmail.com>
2023-11-09 12:42:38 -0500

    imported the logo to the asset folder and used it in the nav, also increased the nav tab size

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Nav.css";
 import SearchBar from "./SearchBar";
+import logo from '../Assets/Logo-Naranja.PNG'
 
 const Nav = () => {
   return (
     <div className="nav__background">
       <nav>
         <figure className="nav__logo">
-            <img src="" alt="" />
+            <img src={logo} alt="" />
         </figure>
         <SearchBar/>
         <div className="nav__shortcuts"></div>
