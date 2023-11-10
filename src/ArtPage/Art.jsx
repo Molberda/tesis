defdee89a1717bf20981d9170f092646bed8f930
David Moller <davidmollera@hotmail.com>
2023-11-10 12:34:19 -0500

    imported the main img for the art category

--- src/ArtPage/Art.jsx
+++ src/ArtPage/Art.jsx
@@ -6,12 +6,10 @@ import { ArtData } from "../Categories/Art";
 const Art = () => {
   return (
     <section id="art" className="top__section">
-      <div className="art__container container top__container">
-        <div className="art__row row">
-          {ArtData.map((item) => (
-            <ArtMap item={item} key={item.id} />
-          ))}
-        </div>
+      <div className="art__container">
+        {ArtData.map((item) => (
+          <ArtMap item={item} key={item.id} />
+        ))}
       </div>
     </section>
   );
--- src/ArtPage/ArtMap.jsx
+++ src/ArtPage/ArtMap.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 const ArtMap = ({ item }) => {
   return (
-    <div className="artMap__background">
+    <>
       <figure className="artMap__img">
         <img src={item.mainImg} alt="" />
       </figure>
       <div className="artMap__text">
-        <h1>{item.name}</h1>
       </div>
-    </div>
+    </>
   );
 };
 
