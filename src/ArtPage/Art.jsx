b2598cdd311d3c846a759faea1487e26833d5c78
David Moller <davidmollera@hotmail.com>
2023-11-10 11:41:43 -0500

    added basic html for the artmap jsx file, created general css properties for the section top section, containers and rows

--- src/ArtPage/Art.jsx
+++ src/ArtPage/Art.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import './Art.css'
-import ArtMap from './ArtMap';
-import { ArtData } from '../Categories/Art';
-
+import React from "react";
+import "./Art.css";
+import ArtMap from "./ArtMap";
+import { ArtData } from "../Categories/Art";
 
 const Art = () => {
-    return (
-        <section id="art">
-            {ArtData.map((item) => <ArtMap item={item} key={item.id}/>)}
-        </section>
-    );
-}
+  return (
+    <section id="art" className="top__section">
+      <div className="art__container container top__container">
+        <div className="art__row row">
+          {ArtData.map((item) => (
+            <ArtMap item={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Art;
--- src/ArtPage/ArtMap.jsx
+++ src/ArtPage/ArtMap.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const ArtMap = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+const ArtMap = ({ item }) => {
+  return (
+    <div className="artMap__background">
+      <figure className="artMap__img">
+        <img src={item.mainImg} alt="" />
+      </figure>
+      <div className="artMap__text">
+        <h1>{item.name}</h1>
+      </div>
+    </div>
+  );
+};
 
 export default ArtMap;
