c4badf2519486d1e3cc23556fd82ba365cf72a88
David Moller <davidmollera@hotmail.com>
2023-11-14 11:08:40 -0500

    imported the arfocus page to the animated routes file and set up the params for the artfocus to didsplay the data of the desired project

--- src/ArtPage/ArtFocus.jsx
+++ src/ArtPage/ArtFocus.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './ArtFocus.css'
+import { useParams } from 'react-router-dom';
+import { ArtData } from '../Categories/Art';
 
 const ArtFocus = () => {
+
+    const { id } = useParams()
+    const project = ArtData.find((i) => i.id === +id)
+
     return (
-        <div>
-            
-        </div>
+        <section className='top__section'>
+            <h1>{project.name}</h1>
+        </section>
     );
 }
 
--- src/Components/AnimatedRoutes.jsx
+++ src/Components/AnimatedRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../HomePage/Home';
 import Art from '../ArtPage/Art';
+import ArtFocus from '../ArtPage/ArtFocus';
 
 const AnimatedRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/arte' element={<Art/>}/>
+            <Route path='/arte/:id' element={<ArtFocus/>}/>
         </Routes>
     );
 }
