bc35257ff25e960282d47a734b51abf13cecafd2
David Moller <davidmollera@hotmail.com>
2023-11-14 09:39:23 -0500

    added a hover effect for the artmap pages

--- src/ArtPage/ArtMap.jsx
+++ src/ArtPage/ArtMap.jsx
@@ -1,14 +1,16 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 const ArtMap = ({ item }) => {
   return (
-    <>
+    <div className="artMap__container">
       <figure className="artMap__img">
         <img src={item.mainImg} alt="" />
       </figure>
-      <div className="artMap__text">
+      <div className="artMap__hover">
+        <Button>Ver Projecto</Button>
       </div>
-    </>
+    </div>
   );
 };
 
