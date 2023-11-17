d83e6495e2fcb2679c0e39c4bd7aed80872c859b
David Moller <davidmollera@hotmail.com>
2023-11-17 15:49:13 -0500

    added animations for the highlight map component using framer motion and eventlisteners

--- src/HomePage/HighlightMap.jsx
+++ src/HomePage/HighlightMap.jsx
@@ -1,30 +1,47 @@
-import React from "react";
-import { motion as m } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import { motion as m, useAnimation, useInView } from "framer-motion";
 
 const HighlightMap = ({ item }) => {
   const isOdd = item.id % 2 !== 0;
+
+  const ref = useRef();
+  const isInView = useInView(ref);
+  const mainControls = useAnimation()
+
+  useEffect(() => {
+    if(isInView){
+        mainControls.start('show')
+    }
+    else{
+        mainControls.start('hidden')
+    }
+  }, [isInView]);
   return (
     <>
       {isOdd ? (
         <m.figure
+          ref={ref}
           variants={{
-            hidden: { opacity: 0, y: -500 },
-            show: { opacity: 1, y: 0 },
+            hidden: { opacity: 0, x: '75%' },
+            show: { opacity: 1, x: 0 },
           }}
           initial="hidden"
-          animate="show"
+          animate={mainControls}
+          transition={{ duration: 1, ease: 'easeOut' }}
           className="highlightMap__img"
         >
           <img src={item.img} alt="" />
         </m.figure>
       ) : (
         <m.figure
+          ref={ref}
           variants={{
-            hidden: { opacity: 0, y: 500 },
-            show: { opacity: 1, y: 0 },
+            hidden: { opacity: 0, x: '-75%' },
+            show: { opacity: 1, x: 0 },
           }}
           initial="hidden"
-          animate="show"
+          animate={mainControls}
+          transition={{ duration: 1, ease: 'easeInOut' }}
           className="highlightMap__img"
         >
           <img src={item.img} alt="" />
--- src/HomePage/Highlights.jsx
+++ src/HomePage/Highlights.jsx
@@ -6,6 +6,7 @@ import HighlightMap from "./HighlightMap";
 const Highlights = () => {
   return (
     <section id="highlights">
+        <h1 className="highlights__title">Eventos Destacados este Mes</h1>
       <div className="highlights__organizer">
         {HighlightData.map((item) => (
           <HighlightMap item={item} key={item.id} />
