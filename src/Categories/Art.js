d44bba1e1ee03b9dd26fc9b461fe05308a500bbb
David Moller <davidmollera@hotmail.com>
2023-11-16 21:59:08 -0500

    added artproject #4 to the art file

--- src/Categories/Art.js
+++ src/Categories/Art.js
@@ -1,6 +1,7 @@
 import arte1Main from "../Assets/Arte1/Arte1Main.jpeg";
 import arte2Main from "../Assets/Arte2/Arte2Main.png";
 import arte3Main from "../Assets/Arte3/Arte3Main.jpeg";
+import arte4Main from "../Assets/Arte4/Arte4Main.jpeg";
 import arte2Aut from "../Assets/Arte2/Arte2Aut.jpeg";
 import arte3Aut from "../Assets/Arte3/Arte3Aut.jpg";
 
@@ -54,17 +55,27 @@ export const ArtData = [
     description2: "",
   },
   {
-    id: 2,
+    id: 3,
     name: "Viceversa en el MAMBO",
-    artist1: "Maria Morales",
-    artist1Img: arte3Aut,
-    mainImg: arte3Main,
+    artist1: "Ana Maria Devis",
+    artist1Img: '',
+    artist2: "Antonio Caro",
+    artist2Img: "",
+    artist3: "Beatriz Gonzalez",
+    artist3Img: "",
+    artist4: "Jose Alejandro Restrepo",
+    artist4Img: "",
+    artist5: "Adriana Marmorek",
+    artist5Img: "",
+    artist6: "Juan Emanuel Echavarria",
+    artist6Img: "",
+    mainImg: arte4Main,
     addImg1: "",
     addImg2: "",
     addImg3: "",
-    subtitle: "En colaboración con Arturo Gutiérrez y Pelitoz locos",
+    subtitle: "Para la celebración de los 60 años del Museo de Arte Moderno de Bogotá llega Viceversa, una exposición que resalta la Colección del Museo",
     description1:
-      "Entrar en el espacio del arte, como en un templo, es creer que la disposición de los objetos y las imágenes cumplen una función mágica. Ánima Híbrida invita a participar en este espacio desde la experiencia material que nos atraviesa a todos y a todas. Sentir el agua y la tierra, contemplar las formas de la luz, mezclarse con la energía de la vida que trae el chacal desde el fondo de la laguna, que también es la muerte.",
-    description2: "",
+      " El Museo ha preparado una exposición que resalta los pilares fundamentales de la institución, reflejando el pasado, el presente y el proyecto para el futuro; Viceversa sugiere interpretaciones alternas que desafían las narrativas visuales convencionales adoptando un enfoque “a la inversa” al tiempo que propone conexiones, contrapuntos visuales y conceptuales inusuales.",
+    description2: "Además, esta exposición reúne más de 400 obras y cuenta con tres proyectos de sitio específico de artistas de procedencias geográficas e inquietudes diferentes, y que ofrecen nuevas perspectivas sobre la historia, la misión y la agencia del Museo: Genealogía de una colección de la argentina Adriana Bustos; Saludos desde Bogotá del italiano Giuseppe Stampone; y Los museos también lloran (y harto) del colombiano Juan Uribe.",
   },
 ];
