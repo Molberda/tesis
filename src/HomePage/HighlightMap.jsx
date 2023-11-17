b0fed067e3ac24b507781f51fbf764ac837a0fa7
David Moller <davidmollera@hotmail.com>
2023-11-17 14:18:58 -0500

    testing the observer

--- src/HomePage/HighlightMap.jsx
+++ src/HomePage/HighlightMap.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
 const HighlightMap = ({ item }) => {
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if(entry.isIntersecting){
+                entry.target.classList.add('show')
+            }
+            else {
+                entry.target.classList.remove('show')
+            }
+        })
+    })
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el))
+
     return (
-        <figure className="highlightMap__img">
+        <figure className="highlightMap__img hidden">
             <img src={item.img} alt="" />
         </figure>
     );
--- src/HomePage/Highlights.jsx
+++ src/HomePage/Highlights.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import './Highlights.css'
-import { HighlightData } from '../Categories/HighlightsData';
-import HighlightMap from './HighlightMap';
+import React from "react";
+import "./Highlights.css";
+import { HighlightData } from "../Categories/HighlightsData";
+import HighlightMap from "./HighlightMap";
 
 const Highlights = () => {
 
-    const hiddenElements = document.querySelectorAll('.hidden')
-
-    return (
-        <section id="highlights">
-            {HighlightData.map((item) => (<HighlightMap item={item} key={item.id}/>))}
-        </section>
-    );
-}
+  return (
+    <section id="highlights">
+      <div className="highlights__organizer">
+        {HighlightData.map((item) => (
+          <HighlightMap item={item} key={item.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Highlights;
