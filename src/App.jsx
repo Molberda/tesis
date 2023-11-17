c8e02f9dec3186e62318672b51d437e1977bb136
David Moller <davidmollera@hotmail.com>
2023-11-17 16:19:59 -0500

    created a scroll to top component so that the user is always redirected to the top of each new page for the best experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import AnimatedRoutes from "./Components/AnimatedRoutes";
 import Nav from "./Components/Nav";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop/>
       <Nav />
       <AnimatedRoutes />
     </Router>
