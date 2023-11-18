686362b8fb3dca7f5a86fe4589f6a0628e96b4de
David Moller <davidmollera@hotmail.com>
2023-11-18 12:50:18 -0500

    updates to home css and imported motion to the home component to animate it

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Landing from "./Landing";
 import Highlights from "./Highlights";
+import "./Home.css";
+import { motion as m } from "framer-motion";
 
 const Home = () => {
   return (
-    <>
+    <m.section id="home">
       <Landing />
       <Highlights />
-    </>
+    </m.section>
   );
 };
 
