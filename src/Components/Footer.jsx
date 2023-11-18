d051693142dd828b46454366022d5bdfc8d8d9a2
David Moller <davidmollera@hotmail.com>
2023-11-18 12:40:54 -0500

    added basic html for the footer component and styled it in the footer css file

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <footer>
-            
-        </footer>
-    );
-}
+  return (
+    <footer>
+      <div className="footer__row row">
+        <div className="footer__categories">
+          <li>
+            <Link to={"/arte"}>Arte</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/diseno"}>Diseno</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/moda"}>Moda</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/teatro"}>Teatro</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/musica"}>Musica</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+        </div>
+        <div className="footer__artists"></div>
+        <div className="footer__contact"></div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
