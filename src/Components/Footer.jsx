4daee7b7f484ffe61fd52defc00eda6bcdd4ee1b
David Moller <davidmollera@hotmail.com>
2023-11-20 03:07:04 -0500

    changed the way in which highlights are displayed to display 2 rows of 5 highlights and added more css and html to the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
     <footer>
       <div className="footer__row row">
         <div className="footer__categories">
+          <h1>Categorias</h1>
           <li>
             <Link to={"/arte"}>Arte</Link>
             <div className="footer__hover--effect"></div>
@@ -28,8 +29,52 @@ const Footer = () => {
             <div className="footer__hover--effect"></div>
           </li>
         </div>
-        <div className="footer__artists"></div>
-        <div className="footer__contact"></div>
+        <div className="footer__artists">
+          <h1>Categorias</h1>
+          <li>
+            <Link to={"/arte"}>Arte</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/diseno"}>Diseno</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/moda"}>Moda</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/teatro"}>Teatro</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/musica"}>Musica</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+        </div>
+        <div className="footer__contact">
+          <h1>Categorias</h1>
+          <li>
+            <Link to={"/arte"}>Arte</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/diseno"}>Diseno</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/moda"}>Moda</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/teatro"}>Teatro</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+          <li>
+            <Link to={"/musica"}>Musica</Link>
+            <div className="footer__hover--effect"></div>
+          </li>
+        </div>
       </div>
     </footer>
   );
