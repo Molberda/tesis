1eb93d7a9584e6d313f732891a273c6867fee69d
David Moller <davidmollera@hotmail.com>
2023-11-20 03:45:25 -0500

    created the moda data file and imported all of the necessary files to assets folder corresponding to each apparel folder

---
+++ src/Categories/Moda.js
@@ -0,0 +1,55 @@
+import apa1img from "../Assets/Moda/Apparel/apa1/apa1img.jpeg";
+import apa1logo from "../Assets/Moda/Apparel/apa1/apa1logo.png";
+import apa2img from "../Assets/Moda/Apparel/apa2/apa2img.webp";
+import apa2logo from "../Assets/Moda/Apparel/apa2/apa2logo.webp";
+import apa3img from "../Assets/Moda/Apparel/apa3/apa3img.webp";
+import apa3logo from "../Assets/Moda/Apparel/apa3/apa3logo.webp";
+import apa4img from "../Assets/Moda/Apparel/apa4/apa4img.webp";
+import apa4logo from "../Assets/Moda/Apparel/apa4/apa4logo.png";
+
+export const ModaData = [
+  {
+    id: 0,
+    type: "APPAREL",
+    name: "A Modo Mio",
+    img: apa1img,
+    logo: apa1logo,
+    link: "https://amodomio.com.co",
+    ig: "https://instagram.com/amodomiostore?igshid=OGQ5ZDc2ODk2ZA==",
+    description:
+      "En A MODO MIO crean piezas únicas cultivadas en Colombia, son una marca que utiliza la moda como un catalizador de mensajes que reflejan su  filosofía y sus creencias. Diseñan prendas con mucho poderío estético que buscan retar la cultura y acabar con los estigmas que les impiden a las mujeres ser libres y ser lo que quieren ser Sus diseños mezclan técnicas de confección, bordados, pedrería, texturas y colores creando piezas cargadas de emociones para que cada mujer se exprese libremente con prendas originales, diferentes, y únicas; las caracterizan a través de piezas que llaman la atención porque no son convencionales, son especiales que les permiten a las mujeres manifestar su belleza. Trabajan libremente porque les gusta lo  que hacen.. A MODO MIO  es una invitación permanente para vivir A MI MANERA.",
+  },
+  {
+    id: 1,
+    type: "APPAREL",
+    name: "AGYBO",
+    img: apa2img,
+    logo: apa2logo,
+    link: "https://agybo.com",
+    ig: "https://instagram.com/agybo?igshid=OGQ5ZDc2ODk2ZA==",
+    description:
+      "AGYBO nace en el 2021 con la misión de llenar el mundo de amor, gratitud y buena onda, creando productos para usar todos los días y verse bien en cualquier ocasión; usan materiales de alta gama para garantizar productos más cómodos, suaves y duraderos.",
+  },
+  {
+    id: 2,
+    type: "APPAREL",
+    name: "Ciclico",
+    img: apa3img,
+    logo: apa3logo,
+    link: "https://ciclico.com.co",
+    ig: "https://instagram.com/ciclico___?igshid=OGQ5ZDc2ODk2ZA==",
+    description:
+      "Simple, suficiente, atemporal. Una marca que tiene ropa, cosas de hogar y accesorios; se caracteriza por usar residuos textiles y botellas PET, materiales que de otra manera no tendrían valor, convirtiéndolos en productos 100% reciclados y sostenibles. Dentro de su proceso de selección de materiales entendieron que el algodón sería la clave ya que es la tela más usada, menos reciclada y una fibra que requiere grandes cantidades de agua para crecer. Adicional a esto, son una marca que no usa tinturas, gasta menos energía, genera menos emisiones de CO2, es 100% Colombiano y apoya a madres cabeza de familia.",
+  },
+  {
+    id: 3,
+    type: "APPAREL",
+    name: "Madre Tierra",
+    img: apa4img,
+    logo: apa4logo,
+    link: "https://madretierra.com.co",
+    ig: "https://instagram.com/madretierra.co?igshid=OGQ5ZDc2ODk2ZA==",
+    description:
+      "Es una marca de ropa ecológica Colombiana; desde su inicio creen que la industria de la moda necesitaba una marca de ropa sostenible y con estilo. Una marca que  permita vestir para la vida diaria sin abandonar el sueño de ser más conscientes de la importancia de la sostenibilidad. Creen que trabajando en equipo se puede descontaminar el agua, reciclar, eliminar los desechos tóxicos, reducir las emisiones de carbono y utilizar menos petróleo. Además, creen que la industria textil puede ser constructiva y sueñan con un planeta en el que tejemos, diseñamos y vestimos lo que creemos.",
+  },
+];
