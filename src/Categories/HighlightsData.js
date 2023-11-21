79fa2f3acf484f483f95e77e872b63fd7309f582
David Moller <davidmollera@hotmail.com>
2023-11-21 11:34:18 -0500

    created the slideshow component

--- src/Categories/HighlightsData.js
+++ src/Categories/HighlightsData.js
@@ -1,51 +1,63 @@
-import img1 from '../Assets/Highlights/highlight1.jpeg'
-import img2 from '../Assets/Highlights/highlight2.jpeg'
-import img3 from '../Assets/Highlights/highlight3.webp'
-import img4 from '../Assets/Highlights/highlight4.jpeg'
-import img5 from '../Assets/Highlights/highlight5.jpeg'
-import img6 from '../Assets/Highlights/highlight6.jpeg'
-import img7 from '../Assets/Highlights/highlight7.jpeg'
-import img8 from '../Assets/Highlights/highlight8.jpeg'
+import img1 from "../Assets/Highlights/highlight1.jpeg";
+import img2 from "../Assets/Highlights/highlight2.jpeg";
+import img3 from "../Assets/Highlights/highlight3.webp";
+import img4 from "../Assets/Highlights/highlight4.jpeg";
+import img5 from "../Assets/Highlights/highlight5.jpeg";
+import img6 from "../Assets/Highlights/highlight6.jpeg";
+import img7 from "../Assets/Highlights/highlight7.jpeg";
+import img8 from "../Assets/Highlights/highlight8.jpeg";
+import img9 from "../Assets/Highlights/highlight9.jpg";
+import img10 from "../Assets/Highlights/highlight10.jpg";
 
 export const HighlightData = [
-    {
-        id: 0,
-        name: 'TIMØ',
-        img: img1,
-    },
-    {
-        id: 1,
-        name: 'Classic Stone',
-        img: img2,
-    },
-    {
-        id: 2,
-        name: 'Carlos Vives',
-        img: img3,
-    },
-    {
-        id: 3,
-        name: 'Primavera Sound',
-        img: img4,
-    },
-    {
-        id: 4,
-        name: 'Noche de Melodia y Bolero',
-        img: img5,
-    },
-    {
-        id: 5,
-        name: 'Mujeres a la Plancha',
-        img: img6,
-    },
-    {
-        id: 6,
-        name: 'Roger Waters',
-        img: img7,
-    },
-    {
-        id: 7,
-        name: 'Lo Que Pasa en el Bar',
-        img: img8,
-    },
-]
+  {
+    id: 0,
+    name: "TIMØ",
+    img: img1,
+  },
+  {
+    id: 1,
+    name: "Classic Stone",
+    img: img2,
+  },
+  {
+    id: 2,
+    name: "Fashion Fest",
+    img: img10,
+  },
+  {
+    id: 3,
+    name: "Primavera Sound",
+    img: img4,
+  },
+  {
+    id: 4,
+    name: "Noche de Melodia y Bolero",
+    img: img5,
+  },
+  {
+    id: 5,
+    name: "Mujeres a la Plancha",
+    img: img6,
+  },
+  {
+    id: 6,
+    name: "Roger Waters",
+    img: img7,
+  },
+  {
+    id: 7,
+    name: "Lo Que Pasa en el Bar",
+    img: img8,
+  },
+  {
+    id: 8,
+    name: "Feria Centipeda",
+    img: img9,
+  },
+  {
+    id: 9,
+    name: "Carlos Vives",
+    img: img3,
+  },
+];
